1f03cfd33abf04c1e85a0c7e3e9aa252ec359b59
7echkilla <prithik1510@gmail.com>
2026-01-03 18:11:15 +1100

    Bug fix

--- gun.py
+++ gun.py
@@ -7,8 +7,8 @@ class Gun(pygame.sprite.Sprite):
     def __init__(self, screen_width, screen_height):
         super().__init__()
 
-        self._width = screen_width // 10
-        self._height = screen_height // 10
+        width = screen_width // 10
+        height = screen_height // 10
 
         self._vertical_minimum = 0
         self._vertical_maximum = screen_height
@@ -17,10 +17,8 @@ class Gun(pygame.sprite.Sprite):
         image_path = os.path.join(base_path, "assets", "cannon.png")
         sound_path = os.path.join(base_path, "assets", "silencer.mp3")
 
-        # self._image_path = os.path.join(os.path.realpath(os.path.dirname(__file__)), "assets", "cannon.png")
-        # self._sound_path = os.path.join(os.path.realpath(os.path.dirname(__file__)), "assets", "silencer.mp3")
-        self.image = pygame.transform.scale(pygame.image.load(self._image_path).convert_alpha(), (self._width, self._height))
-        self._sound = pygame.mixer.Sound(self.sound_path)
+        self.image = pygame.transform.scale(pygame.image.load(image_path).convert_alpha(), (width, height))
+        self._sound = pygame.mixer.Sound(sound_path)
 
         self.rect = self.image.get_rect()
         self.rect.left = screen_width // 20
@@ -29,14 +27,14 @@ class Gun(pygame.sprite.Sprite):
         self._speed = 6
 
     def update(self, keys):
-        if keys[pygame.K_w] and self._rect.top > self._vertical_minimum:
-            self._rect.y -= self._speed
-        if keys[pygame.K_s] and self._rect.bottom < self._vertical_maximum:
-            self._rect.y += self._speed
+        if keys[pygame.K_w] and self.rect.top > self._vertical_minimum:
+            self.rect.y -= self._speed
+        if keys[pygame.K_s] and self.rect.bottom < self._vertical_maximum:
+            self.rect.y += self._speed
 
     def shoot(self):
         self._sound.play()
-        bullet = Bullet(self._rect.right, self._rect.centery)
+        bullet = Bullet(self.rect.right, self.rect.centery)
         return bullet
     
 if __name__ == "__main__":
